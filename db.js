2ac7b105c144862913810b255356bdb2243ebbac
darthnator <darthnator@gmail.com>
2023-10-25 22:31:26 -0600

    add encodeURICOmponent

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGODB_URI;
+const uri = encodeURIComponent(process.env.MONGODB_URI);
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
